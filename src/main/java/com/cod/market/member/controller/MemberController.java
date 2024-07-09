c762bc8997d506fbef7ac26f0312971e88253abb
hyunji1205 <sguswl1205@gmail.com>
2024-07-09 15:19:00 +0900

    spring security

--- src/main/java/com/cod/market/member/controller/MemberController.java
+++ src/main/java/com/cod/market/member/controller/MemberController.java
@@ -2,7 +2,19 @@ package com.cod.market.member.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/member")
 public class MemberController {
+    @GetMapping("/login")
+    public String login() {
+        return "member/login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "member/join";
+    }
 }
--- src/main/java/com/cod/market/member/repository/MemberRepository.java
+++ src/main/java/com/cod/market/member/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.cod.market.member.repository;
 
+import com.cod.market.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @ResponseBody
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 }
