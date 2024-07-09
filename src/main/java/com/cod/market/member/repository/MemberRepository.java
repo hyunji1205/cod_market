fc89714a32ae4fcdc6eb8f99bc39ead5052c80d1
hyunji1205 <sguswl1205@gmail.com>
2024-07-09 16:44:18 +0900

    login security

--- src/main/java/com/cod/market/member/repository/MemberRepository.java
+++ src/main/java/com/cod/market/member/repository/MemberRepository.java
@@ -4,6 +4,11 @@ import com.cod.market.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Optional;
+
 @ResponseBody
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+
+
 }
--- src/main/java/com/cod/market/security/SecurityConfig.java
+++ src/main/java/com/cod/market/security/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.cod.market.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -26,5 +28,10 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
