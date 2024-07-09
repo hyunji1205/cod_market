e83e8c8707c85c5a013d6cdbb51fce2d82ff7162
hyunji1205 <sguswl1205@gmail.com>
2024-07-09 16:47:06 +0900

    login security 수정

--- src/main/java/com/cod/market/security/SecurityConfig.java
+++ src/main/java/com/cod/market/security/SecurityConfig.java
@@ -11,7 +11,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -20,7 +19,11 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
-                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .formLogin((formLogin) -> formLogin
+                        .loginPage("/member/login")
+                        .defaultSuccessUrl("/"))
+        ;
 
         return http.build();
     }
