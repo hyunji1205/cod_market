ad2dc59c58e2f2d53e6ba48a898f245dfa9151e5
hyunji1205 <sguswl1205@gmail.com>
2024-07-10 17:20:57 +0900

    question mvc complete

--- src/main/java/com/cod/market/member/service/MemberService.java
+++ src/main/java/com/cod/market/member/service/MemberService.java
@@ -7,12 +7,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+
     public Member join(String username, String password, String email, String nickname) {
         Member member = new Member();
         member.setUsername(username);
@@ -23,5 +25,15 @@ public class MemberService {
 
         memberRepository.save(member);
         return member;
+    }
+
+    public Member findByUserName(String username) {
+        Optional<Member> member = memberRepository.findByUsername(username);
+
+        if ( member.isPresent() ) {
+            return member.get();
+        }  else {
+            throw new RuntimeException("member not found");
+        }
     }
 }
