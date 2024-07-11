0b63171e273ea498b27e32e5ae0c611e8e485d7c
hyunji1205 <sguswl1205@gmail.com>
2024-07-11 15:01:38 +0900

    question modify

--- src/main/java/com/cod/market/question/service/QuestionService.java
+++ src/main/java/com/cod/market/question/service/QuestionService.java
@@ -8,12 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class QuestionService {
     private final QuestionRepository questionRepository;
-
 
     public void create(Product product, Member member, String content) {
         Question q = new Question();
@@ -22,5 +22,22 @@ public class QuestionService {
         q.setContent(content);
         q.setCreateDate(LocalDateTime.now());
         questionRepository.save(q);
+    }
+
+    public Question getQuestion(Long id) {
+        Optional<Question> question = questionRepository.findById(id);
+
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new RuntimeException("question not found");
+        }
+    }
+
+    public void modify(Question question, String content) {
+        question.setContent(content);
+        question.setModifyDate(LocalDateTime.now());
+
+        questionRepository.save(question);
     }
 }
