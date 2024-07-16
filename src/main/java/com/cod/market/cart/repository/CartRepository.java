fad9b024f0b10a17058eca5423807603cff43431
hyunji1205 <sguswl1205@gmail.com>
2024-07-16 16:26:02 +0900

    edit entity for payment

--- src/main/java/com/cod/market/cart/repository/CartRepository.java
+++ src/main/java/com/cod/market/cart/repository/CartRepository.java
@@ -1,6 +1,6 @@
 package com.cod.market.cart.repository;
 
-import com.cod.market.cart.entity.Cart;
+import com.cod.market.cart.entity.CartItem;
 import com.cod.market.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<CartItem, Long> {
 
-    List<Cart> findByMember(Member member);
+    List<CartItem> findByMember(Member member);
 }
